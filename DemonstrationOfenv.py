60952081378f1136967d4589e6e9853111aa1bd0
jayakrishna662 <jayakrishna7828@gmail.com>
2026-03-07 12:19:32 +0530

    second improved  DemonstrationOfenv

--- DemonstrationOfenv.py
+++ DemonstrationOfenv.py
@@ -1,8 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()   # loads values from .env file
+load_dotenv()  # loads values from .env file
 
 db_pass = os.getenv("DB_PASSWORD")
-
 print(db_pass)
